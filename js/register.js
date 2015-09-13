994d980245e4e257293445bb933228bfcd950204
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-09-14 00:45:55 +0800

    ConfirmCodeTimer

--- js/register.js
+++ js/register.js
@@ -1,11 +1,39 @@
 /**
  * Created by truemenhale on 15/8/23.
  */
-var flag = true;
+function getCookie(name)
+{
+	var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+	if(arr = document.cookie.match(reg))
+		return unescape(arr[2]);
+	else
+		return null;
+}
 $(function(){
+	if(getCookie("flag") == 500){
+		time = parseInt(getCookie('time'));
+		_this = $('.ApplyBtn');
+		_this.html("等待"+time+"秒后重新发送");
+		_this.css({"color":"#ccc","text-shadow":"none"});
+		var timer = setInterval(function(){
+			time--;
+			document.cookie = "time = "+time;
+			if(time <= 0){
+				_this.html("获取验证码");
+				_this.css({"color":"#fff","text-shadow":"inset"});
+				document.cookie = "flag = true";
+				clearInterval(timer);
+				return;
+			}
+			_this.html("等待"+time+"秒后重新发送");
+		},1000);
+
+	}
 	$('.ApplyBtn').on('tap',function(){
 		var _this = $(this);
-		if(flag == true){
+		if(getCookie("flag") == "true"||!getCookie("flag")){
+			document.cookie = "flag = true";
+			document.cookie = "time = "+5;
 			if($('.phoneNum').val().length != 11){
 				alert('请输入正确的手机号！');
 				return;
@@ -13,24 +41,25 @@ $(function(){
 			$.post('./api/index.php?s=/Home/Account/codeSend','phone='+$('.phoneNum').val(),function(data){
 
 			});
-			time = 60;
-			flag = false;
+			time = parseInt(getCookie('time'));
+			document.cookie = "flag = false";
 		}
-		if(flag == false){
+		if(getCookie("flag") == "false"){
 			_this.html("等待"+time+"秒后重新发送");
 			_this.css({"color":"#ccc","text-shadow":"none"});
 			var timer = setInterval(function(){
 							time--;
+				document.cookie = "time = "+time;
 							if(time == 0){
 								_this.html("获取验证码");
 								_this.css({"color":"#fff","text-shadow":"inset"});
-								flag = true;
+								document.cookie = "flag = true";
 								clearInterval(timer);
 								return;
 							}
 							_this.html("等待"+time+"秒后重新发送");
 						},1000);
-			flag = 500;
+			document.cookie = "flag = "+500;
 		}
 	});
 	$('.registerBtn').on('tap',function(){
