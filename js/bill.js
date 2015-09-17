6f88e04175f5a1b4374b0926a29cbe4e131e6f70
DeadSoul <zygtzxz@outlook.com>
2015-09-17 15:17:05 +0800

    bill update

--- js/bill.js
+++ js/bill.js
@@ -6,8 +6,9 @@ var billInfoUrl = "./api/index.php?s=/Home/Account/billInfo";
 $(function () {
 	$('.getAddress').on('tap',function(){
 		var _data = {};
-		_data.num = $('.ApplyNum').val();
-		_data.address = $('.getAddress').val();
+		_data.phone = ""; //TODO
+		_data.money = $('.ApplyNum').val();
+		_data.addr = $('.getAddress').val();
 		$.post('./api/index.php?s=/Home/Account/applyBill',_data,function(data){
 			if(data>=0){
 				alert('发票申请成功请等候收件！');
