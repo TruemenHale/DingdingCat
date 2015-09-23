f04defbd61cdda656f7a22c960702c10fc95a9b6
DeadSoul <zygtzxz@outlook.com>
2015-09-23 16:12:06 +0800

    wechat test

---
+++ api/Application/Home/Controller/WeixinController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DeadSoul
+ * Date: 2015/9/20
+ * Time: 15:25
+ */
+
+namespace Home\Controller;
+use Com\Wechat;
+use THink\Controller;
+
+class WeixinController extends Controller {
+    private $appId = "wx33717b4ef804de31";
+
+    private $appSecret = '62d1a73eb4c3c141ac0758970d12906e';
+
+    public function enter () {
+        $WeChat = new Wechat('dingdingCat');
+        $WeChat->replyText("test");
+    }
+}
