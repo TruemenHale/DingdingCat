4c4ff5aea1af5e19b5e0edf2a29fcf1b88d642c1
DeadSoul <zygtzxz@outlook.com>
2015-10-01 14:32:56 +0800

    test

--- wxpay/index.php
+++ wxpay/index.php
@@ -1,13 +1,8 @@
 <?php
     session_start();
     $orderNo = $_GET['orderNo'];
+    $money = $_GET['money'];
 
-    $mysqli = new mysqli();
-    $mysqli->connect('deadsoul.mysql.rds.aliyuncs.com','dingdingcat','dingdingcat','dingdingcat');
-
-    $res = $mysqli->prepare("SELECT * FROM orders WHERE orderNo = $orderNo");
-    $res->execute();
-    $res->bind_result($money);
     while ($res->fetch()) {
         echo $money;
         $_SESSION['money'] = $money;
