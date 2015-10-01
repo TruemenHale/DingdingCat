1262d474009fc22b656affb41cc3bd8d9895a36e
DeadSoul <zygtzxz@outlook.com>
2015-10-01 14:51:23 +0800

    test

--- wxpay/index.php
+++ wxpay/index.php
@@ -1,20 +1,12 @@
 <?php
     session_start();
     $orderNo = $_GET['orderNo'];
-    $url = "../api/index.php?s=/Home/Order/orderToMoney";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-    // post数据
-    curl_setopt($ch, CURLOPT_POST, 1);
-    // post的变量
-    curl_setopt($ch, CURLOPT_POSTFIELDS, 'orderNo='.$orderNo);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    $res = json_decode($output, TRUE);
-    $money = $res['money'];
+
+    $pdo = new PDO("mysql:host=deadsoul.mysql.rds.aliyuncs;dbname=dingdingcat","dingdingcat","dingdingcat");
+    $rs = $pdo -> query("SELECT * FROM orders WHERE orderNo = $orderNo");
+    while($row = $rs -> fetch()) {
+        print_r($row);
+    }
     echo "订单:".$orderNo."\n";
-    echo "价格:".$money
+
 ?>
