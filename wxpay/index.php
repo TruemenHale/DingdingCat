a3d4369a83598135f623528087d214b15afb6643
DeadSoul <zygtzxz@outlook.com>
2015-10-01 14:53:49 +0800

    test

--- wxpay/index.php
+++ wxpay/index.php
@@ -1,12 +1,9 @@
 <?php
     session_start();
     $orderNo = $_GET['orderNo'];
-
-    $pdo = new PDO("mysql:host=deadsoul.mysql.rds.aliyuncs;dbname=dingdingcat","dingdingcat","dingdingcat");
-    $rs = $pdo -> query("SELECT * FROM orders WHERE orderNo = $orderNo");
-    while($row = $rs -> fetch()) {
-        print_r($row);
-    }
-    echo "订单:".$orderNo."\n";
-
+    $money   = $_get['money'];
+    session('orderNo',$orderNo);
+    session('money',$money);
+    header("Location: ./example/jsapi.php");
+    exit;
 ?>
