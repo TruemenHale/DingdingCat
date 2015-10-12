eb1fca5f9c833e2786dcbe40b5d806e290bd30dc
DeadSoul <zygtzxz@outlook.com>
2015-10-12 19:45:40 +0800

    update

--- js/usrcenter.js
+++ js/usrcenter.js
@@ -3,6 +3,15 @@ var _url = self.location.href;
 if(_url.indexOf("?")>0){
     var openid = _url.substr(_url.indexOf("?")+1,_url.length-_url.indexOf("?"));
 }
+$(document).on("pagebeforeshow","#Allorder",function(){
+    $('#Allorder').find('.xiangqing').addClass('ui-link ui-btn ui-btn-active');
+});
+$(document).on("pagebeforeshow","#Daisongorder",function(){
+    $('#Daisongorder').find('.daisong').addClass('ui-link ui-btn ui-btn-active');
+});
+$(document).on("pagebeforeshow","#Daigouorder",function(){
+    $('#Daigouorder').find('.daigou').addClass('ui-link ui-btn ui-btn-active');
+});
 
 $(function () {
     $.mobile.loading('show');
@@ -25,8 +34,57 @@ $(function () {
                 document.getElementById('nickname').innerText = response.nickname;
                 document.getElementById('score').innerText = response.score;
                 document.getElementById('phone').innerText = response.phone;
+                orderList(data);
+                commonAddr(data)
+            }
+            $.mobile.loading('hide');
+        }
+    });
+});
+
+function orderList (phone) {
+    $.ajax({
+        type: 'POST',
+        url: './api/index.php?s=/Home/Order/orderList',
+        data: 'phone=' + phone,
+        dataType: 'json',
+        error: function (request) {
+            alert('获取失败')
+        },
+        success: function (response) {
+            var status = response.status;
+            var send   = response.send;
+            var buy    = response.buy;
+
+            if (status != 0) {
+                alert("订单获取失败");
+            } else {
+                $("#all_list").tmpl(response.all).appendTo('#allList');
+                $("#send_list").tmpl(send).appendTo('#sendList');
+                $("#buy_list").tmpl(buy).appendTo('#buyList');
             }
         }
     });
-},100);
+}
 
+function commonAddr (phone) {
+    $.ajax({
+        type: 'POST',
+        url: './api/index.php?s=/Home/Order/commonAddr',
+        data: 'phone=' + phone,
+        dataType: 'json',
+        error: function (request) {
+            alert('获取失败')
+        },
+        success: function (response) {
+            var status = response.status;
+            var list   = response.list;
+            console.log(response);
+            if (status != 0) {
+                alert("订单常用地址失败");
+            } else {
+                $("#addr_list").tmpl(list).appendTo('#addrList');
+            }
+        }
+    });
+}
