718666ea4aa54f998191f5f1ea4265802924191b
DeadSoul <zygtzxz@outlook.com>
2015-10-19 00:36:06 +0800

    update

--- api/Application/Home/Controller/OrderController.class.php
+++ api/Application/Home/Controller/OrderController.class.php
@@ -62,7 +62,7 @@ class OrderController extends BaseController {
             'orderNo'=> $orderNo,
             'payType'=> $info['payType']
         ];
-
+        session("orderNo",$orderNo);
         $this->ajaxReturn($return);
     }
 
--- wxpay/example/jsapi.php
+++ wxpay/example/jsapi.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['money'])) {
 	$_SESSION['money'] = $money;
 }
 
-$orderNo = $_POST['orderNo'];
+$orderNo = $_SESSION['orderNO'];
 
 //①、获取用户openid
 $tools = new JsApiPay();
