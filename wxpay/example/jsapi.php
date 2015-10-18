e397dbd6768e10eaca58ca2c3f9710c6c58eb429
DeadSoul <zygtzxz@outlook.com>
2015-10-19 02:36:02 +0800

    update

--- wxpay/example/jsapi.php
+++ wxpay/example/jsapi.php
@@ -45,7 +45,7 @@ $input->SetTotal_fee($money*100);//*100
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");
-$input->SetNotify_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].$_SERVER['QUERY_STRING'].'/notify.php');
+$input->SetNotify_url('http://wx.tyll.net.cn/DingdingCat/wxpay/example/notify.php');
 $input->SetTrade_type("JSAPI");
 $input->SetOpenid($openId);
 $order = WxPayApi::unifiedOrder($input);
