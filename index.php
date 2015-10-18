a9b91dc521b54ca02c692c9f91466c41602dff0f
DeadSoul <zygtzxz@outlook.com>
2015-10-19 02:16:31 +0800

    update

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
         <div id="daisongPay" style="">
             <form action="./wxpay/example/jsapi.php" method="post" data-ajax="false">
                 <input type="hidden" name="money" id="wxpayMoney" value="0.01">
-                <input type="hidden" name="orderNo" id="wxpayOrder" value="S144518622915207694">
+                <input type="hidden" name="orderNo" id="wxpayOrder" value="S144518607561738735">
                 <input type="submit" id="wxpayBtn" value="前去支付">
             </form>
         </div>
