412897f7fcb762864852665b82aa657077f68cb3
DeadSoul <zygtzxz@outlook.com>
2015-10-28 22:00:18 +0800

    test

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
 
     $errcode = $json_obj ['errcode'];
 
-    if (empty($errcode)) {
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2ftest.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+    if (!empty($errcode)) {
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2findex.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
         header("Location:".$url);
     }
     //根据openid和access_token查询用户信息
