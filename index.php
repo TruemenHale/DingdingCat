61f7837587a9e674944cf60029cac4c0e33aef6e
DeadSoul <zygtzxz@outlook.com>
2015-10-28 21:58:42 +0800

    test

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
     $errcode = $json_obj ['errcode'];
 
-    if ($errcode) {
+    if (empty($errcode)) {
         $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2ftest.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
         header("Location:".$url);
     }
