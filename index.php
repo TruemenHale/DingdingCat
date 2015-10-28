1af21780b67b4bbd6e3d0b33f6f7abe08810f36c
DeadSoul <zygtzxz@outlook.com>
2015-10-28 22:50:45 +0800

    test

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 
     $appid = "wxcb5b14c964fadb27";
     $secret = "7cfbf146c18280d071d6e97a15f0acb7";
-    $code = $_GET["code"];
 
-    if (is_null($code)) {
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2findex.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+    if (isset($_GET['code'])){
+        $code = $_GET['code'];
+    }else{
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcb5b14c964fadb27&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2findex.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
         header("Location:".$url);
     }
 
@@ -21,13 +22,6 @@
     $res = curl_exec($ch);
     curl_close($ch);
     $json_obj = json_decode($res,true);
-
-    $errcode = $json_obj ['errcode'];
-
-    if ($errcode) {
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2findex.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-        header("Location:".$url);
-    }
     //根据openid和access_token查询用户信息
     $access_token = $json_obj['access_token'];
     $openid = $json_obj['openid'];
