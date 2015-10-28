beec1edf460e54aabf96e4e0e2c96413c98e6281
DeadSoul <zygtzxz@outlook.com>
2015-10-28 22:46:39 +0800

    test

--- test.php
+++ test.php
@@ -4,9 +4,9 @@ $appid = "wxcb5b14c964fadb27";
 $secret = "7cfbf146c18280d071d6e97a15f0acb7";
 
 if (isset($_GET['code'])){
-    $code =  $_GET['code'];
+    $code = $_GET['code'];
 }else{
-    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2ftest.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcb5b14c964fadb27&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2ftest.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
     header("Location:".$url);
 }
 
