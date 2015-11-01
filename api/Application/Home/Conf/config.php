5834b3aa41edec9d95c4763250425aae500b3384
DeadSoul <zygtzxz@outlook.com>
2015-11-01 21:56:32 +0800

    update

--- api/Application/Home/Conf/config.php
+++ api/Application/Home/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'deadsoul.mysql.rds.aliyuncs.com', // 服务器地址
-    'DB_NAME'   => 'dingdingcat', // 数据库名
-    'DB_USER'   => 'dingdingcat', // 用户名
-    'DB_PWD'    => 'dingdingcat', // 密码
+    'DB_HOST'   => 'kdj.tyll.net.cn', // 服务器地址
+    'DB_NAME'   => 'dingdingmao', // 数据库名
+    'DB_USER'   => 'ddm', // 用户名
+    'DB_PWD'    => 'ddm', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => '', // 数据库表前缀
     'DB_CHARSET'=> 'utf8', // 字符集
