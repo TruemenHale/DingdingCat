5e777790e1544fb7ce6ddeb6ea27002395ed57ac
DeadSoul <zygtzxz@outlook.com>
2015-11-01 15:16:33 +0800

    update

--- bill.php
+++ bill.php
@@ -1,3 +1,34 @@
+<?php
+ini_set('date.timezone','Asia/Shanghai');
+require_once "jssdk/jssdk.php";
+
+$appid = "wxcb5b14c964fadb27";
+$secret = "7cfbf146c18280d071d6e97a15f0acb7";
+
+if (isset($_GET['code'])){
+	$code = $_GET['code'];
+}else{
+	$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcb5b14c964fadb27&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2fbill.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+	header("Location:".$url);
+}
+
+$get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$secret.'&code='.$code.'&grant_type=authorization_code';
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$get_token_url);
+curl_setopt($ch,CURLOPT_HEADER,0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+$res = curl_exec($ch);
+curl_close($ch);
+$json_obj = json_decode($res,true);
+//根据openid和access_token查询用户信息
+$access_token = $json_obj['access_token'];
+$openid = $json_obj['openid'];
+$jsapi = new JSSDK("wxcb5b14c964fadb27","7cfbf146c18280d071d6e97a15f0acb7");
+$signPackage = $jsapi->getSignPackage();
+
+?>
 <!DOCTYPE html>
 <html>
 	<head lang="en">
@@ -7,6 +38,9 @@
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 		<link rel="stylesheet" href="js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css"/>
+		<script>
+			var openid = "<?php echo $openid; ?>";
+		</script>
 		<script src="js/jquery-2.1.4.min.js"></script>
 		<script src="js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
 		<link rel="stylesheet" href="style/bill.css"/>
@@ -47,4 +81,15 @@
 			</div>
 		</div>
 	</body>
+	<script>
+		wx.config({
+			appId: '<?php echo $signPackage["appId"];?>',
+			timestamp: <?php echo $signPackage["timestamp"];?>,
+			nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+			signature: '<?php echo $signPackage["signature"];?>',
+			jsApiList: [
+				'hideAllNonBaseMenuItem'
+			] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+	</script>
 </html>
---
+++ suggestion.php
@@ -0,0 +1,83 @@
+<?php
+ini_set('date.timezone','Asia/Shanghai');
+require_once "jssdk/jssdk.php";
+
+$appid = "wxcb5b14c964fadb27";
+$secret = "7cfbf146c18280d071d6e97a15f0acb7";
+
+if (isset($_GET['code'])){
+  $code = $_GET['code'];
+}else{
+  $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcb5b14c964fadb27&redirect_uri=http%3a%2f%2fwx.tyll.net.cn%2fDingdingCat%2fsuggestion.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+  header("Location:".$url);
+}
+
+$get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$secret.'&code='.$code.'&grant_type=authorization_code';
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$get_token_url);
+curl_setopt($ch,CURLOPT_HEADER,0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+$res = curl_exec($ch);
+curl_close($ch);
+$json_obj = json_decode($res,true);
+//根据openid和access_token查询用户信息
+$access_token = $json_obj['access_token'];
+$openid = $json_obj['openid'];
+$jsapi = new JSSDK("wxcb5b14c964fadb27","7cfbf146c18280d071d6e97a15f0acb7");
+$signPackage = $jsapi->getSignPackage();
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title>叮叮猫</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  <link rel="stylesheet" href="style/style.css"/>
+  <link rel="stylesheet" href="js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css"/>
+  <script>
+    var openid = "<?php echo $openid; ?>";
+  </script>
+  <script src="js/jquery-2.1.4.min.js"></script>
+  <script src="js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  <script src="js/suggestion.js"></script>
+</head>
+<body>
+
+<!--代送页面-->
+<div data-role="page" id="suggestion">
+  <div data-role="header" data-position="fixed">
+    <h1>建议</h1>
+  </div>
+  <div role="main" class="ui-content">
+    <div class="ui-field-contain">
+      <label>建议内容:</label>
+      <textarea style="resize: none;" data-role="none" name="" class="content" cols="30" rows="10"></textarea>
+    </div>
+  </div>
+  <div class="ui-field-contain">
+    <label>建议分类：</label>
+    <select class="type">
+      <option value="0">建议</option>
+      <option value="1">上报异常</option>
+    </select>
+  </div>
+  <input type="button" value="提交建议" class="sApply"/>
+</div>
+</body>
+<script>
+  wx.config({
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      'hideAllNonBaseMenuItem'
+    ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });
+</script>
+</html>
