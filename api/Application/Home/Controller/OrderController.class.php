4ff8f4e08b7a3b9662f83f89cd426cd4fe9ac8dc
DeadSoul <zygtzxz@outlook.com>
2015-11-14 19:28:58 +0800

    test

--- api/Application/Home/Controller/OrderController.class.php
+++ api/Application/Home/Controller/OrderController.class.php
@@ -671,7 +671,7 @@ class OrderController extends BaseController {
         $openid = I('post.openid');
         $order  = I('post.order');
         $weChat = new WechatAuth();
-
+        $weChat->getAccessToken();
         $content = "支付成功！\n\n你的订单号为".$order."的订单已经成功支付，请等待跑腿哥接单！";
         $res = $weChat->sendText($openid,$content);
         $this->ajaxReturn($res);
