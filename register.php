d0362d4ec6e246a9cade03b27bdc02ef9ab3369a
DeadSoul <zygtzxz@outlook.com>
2015-11-22 01:49:10 +0800

    jssdk update

--- register.php
+++ register.php
@@ -85,6 +85,7 @@
 	</body>
 	<script>
 		wx.config({
+			debug: true,
 			appId: '<?php echo $signPackage["appId"];?>',
 			timestamp: <?php echo $signPackage["timestamp"];?>,
 			nonceStr: '<?php echo $signPackage["nonceStr"];?>',
@@ -93,5 +94,11 @@
 				'hideAllNonBaseMenuItem'
 			] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 		});
+		wx.ready(function() {
+			alert(1);
+		})
+		wx.error(function(res) {
+			alert(res);
+		})
 	</script>
 </html>
