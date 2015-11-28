1091f3f1f675252ebe8572df6e000fc69a2bbb95
DeadSoul <zygtzxz@outlook.com>
2015-11-29 03:05:03 +0800

    test

--- jssdk/jssdk.php
+++ jssdk/jssdk.php
@@ -51,7 +51,7 @@ class JSSDK {
       // 如果是企业号用以下 URL 获取 ticket
       // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
       $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
-      $res = json_decode($this->httpGet($url));
+      $res = json_decode(file_get_contents($url));
       $ticket = $res->ticket;
       if ($ticket) {
         $data->expire_time = time() + 7000;
