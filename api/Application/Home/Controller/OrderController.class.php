1604d214df12a4dddc7c0253b5e0d658b387eeb4
DeadSoul <zygtzxz@outlook.com>
2016-01-20 00:04:29 +0800

    upload

--- api/Application/Home/Controller/OrderController.class.php
+++ api/Application/Home/Controller/OrderController.class.php
@@ -844,7 +844,7 @@ class OrderController extends BaseController {
             "color" => "#173177"
         ];
         $send['keyword2'] = [
-            "value" => date("Y-m-d H-i-s",time()),
+            "value" => date("Y-m-d H:i:s",time()),
             "color" => "#173177"
         ];
         $send['remark'] = [
