e651d99546c2fef03c778dc27ecb223da8825d06
DeadSoul <zygtzxz@outlook.com>
2016-01-20 00:10:28 +0800

    upload

--- api/Application/Home/Controller/OrderController.class.php
+++ api/Application/Home/Controller/OrderController.class.php
@@ -822,47 +822,6 @@ class OrderController extends BaseController {
     }
 
     /**
-     * 订单接单后微信模板消息发送
-     */
-    public function accessMsgSend () {
-        $openid = I('post.openid');
-        $order  = I('post.order');
-        $runnerInfo = $this->orderToRunner($order);
-        $runnerName = $runnerInfo ['name'];
-        $runnerPhone = $runnerInfo ['phone'];
-        $weChat = new WechatAuth();
-        $token = $this->tokenJudge();
-        $weChat->tokenWrite($token);
-        $remark = "点击详情，获取取件二维码";
-        $url = "http://wx.tyll.net.cn/DingdingCat/showQrCode.php?order=".$order;
-        $send['first'] = [
-            "value" => "您好，您的订单已被跑腿哥-$runnerName($runnerPhone)接单，请耐心等待跑腿哥上门。。。",
-            "color" => "#173177"
-        ];
-        $send['keyword1'] = [
-            "value" => $order,
-            "color" => "#173177"
-        ];
-        $send['keyword2'] = [
-            "value" => date("Y-m-d H:i:s",time()),
-            "color" => "#173177"
-        ];
-        $send['remark'] = [
-            "value" => $remark,
-            "color" => "#173177"
-        ];
-        $template_id = "lXVgtPiYy21dQ88efpZN6PDqefamoEXrJDPP8zovPXU";
-
-        $res = $weChat->sendTemplate($openid,$template_id,$send,$url);
-        $this->ajaxReturn($res);
-    }
-
-    private function orderToRunner ($order) {
-        $res = M('orders')->field("runner.name,runner.phone")->where("orders.orderNo = '$order'")->join("runner ON runner.id = orders.runnerId")->find();
-        return $res;
-    }
-
-    /**
      * @param int|string $money
      * @return string
      * 通过订单价格计算跑腿哥收入
